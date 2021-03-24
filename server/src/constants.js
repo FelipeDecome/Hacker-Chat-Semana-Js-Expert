bc0a31e8363ddc9973cf48b52b359a9d7ab5e995
Felipe Decome <felipe.decome@hotmail.com>
2021-03-24 18:39:50 -0300

    aula 02

---
+++ server/src/constants.js
@@ -0,0 +1,10 @@
+const constants = {
+    events: {
+        MESSAGE: 'message',
+        NEW_USER_CONNECTED: 'newUserConnected',
+        DISCONNECT_USER: 'disconnectUser',
+        UPDATE_USERS: 'updateUsers'
+    }
+}
+
+export { constants }
